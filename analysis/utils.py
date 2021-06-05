2a13e2dc2e8835aee5b89aff5b12024344d6d2b6
Dani El-Ayyass <dayyass@yandex.ru>
2021-06-05 10:10:58 +0300

    add columns description

---
+++ analysis/utils.py
@@ -0,0 +1,35 @@
+import os
+
+import pandas as pd
+
+
+def path_join(filename: str, path_to_folder: str = "../data/") -> str:
+    """
+    os.path.join with default path_to_data.
+
+    :param str filename: filename.
+    :param str path_to_folder: path to the folder where the filename is located.
+    :return: joined path.
+    :rtype: str
+    """
+
+    return os.path.join(path_to_folder, filename)
+
+
+def load_train(path_to_train_folder: str = "../data/") -> pd.DataFrame:
+    """
+    Load train data.
+
+    :param str path_to_train_folder: path to the folder where the train data is located.
+    :return: train dataframe.
+    :rtype: pd.DataFrame
+    """
+
+    df_application_train = pd.read_csv(
+        path_join(
+            filename="application_train.csv",
+            path_to_folder=path_to_train_folder,
+        ),
+        index_col="SK_ID_CURR",
+    )
+    return df_application_train
